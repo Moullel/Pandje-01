11fe9673ca0b98e58938a6fb0de0f713a7a96b47
Alfa umaro djalo <Alfaumardiallo30@gmail.com>
2026-09-23 15:45:03 -0300

    usuario

---
+++ Git e GItHub.py
@@ -0,0 +1,9 @@
+login=input("Digite seu login: ")
+password=input("Digite sua senha: ")
+print(f"Login: {login}")
+print(f"Senha: {password}")
+if login=="Moullel" and password=="123456":
+    print("Bem vindo ao sistema!")
+else:
+    print("Login ou senha incorretos. Tente novamente.")
+    exit()
