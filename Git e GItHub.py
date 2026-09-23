f2674821caa3260fcee7120efa565d9866671bbb
Alfa umaro djalo <Alfaumardiallo30@gmail.com>
2026-09-23 15:56:23 -0300

    teste de git 2

--- Git e GItHub.py
+++ Git e GItHub.py
@@ -2,6 +2,7 @@ login=input("Digite seu login: ")
 password=input("Digite sua senha: ")
 print(f"Login: {login}")
 print(f"Senha: {password}")
+
 if login=="Moullel" and password=="123456":
     print("Bem vindo ao sistema!")
 else:
