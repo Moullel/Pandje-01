534fdb1fe41813a48b61ea757d587ea68d48c8f0
Moullel <90836373+Moullel@users.noreply.github.com>
2026-09-23 15:58:14 -0300

    teste 4

--- Git e GItHub.py
+++ Git e GItHub.py
@@ -6,5 +6,5 @@ print(f"Senha: {password}")
 if login=="Moullel" and password=="123456":
     print("Bem vindo ao sistema!")
 else:
-    print("Login ou senha incorretos. Tente novamente.")
+    print("Login ou senha incorretos. besta")
     exit()
